af92ea0dc993aa82a93b159941c404ec2eac9366
Jaewook Lee <wookboy00@naver.com>
2024-03-31 18:21:11 +0900

    Feat: initial page, layout created

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '../components/navigation/Navbar.svelte';
+	import Footer from '../components/Footer.svelte';
+</script>
+
+<Navbar />
+<hr class="mb-10" />
+
+<div class="container w-full mx-auto text-gray-900 text-xl">
+	<slot />
+</div>
+
+<hr class="mt-10" />
+<Footer />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from '../components/button/Button.svelte';
+	import Header from '../components/Header.svelte';
+</script>
+
+<div class="mx-4">
+	<Header />
+	<br />
+	<div class="w-full text-center">
+		<Button text="후원 문의" />
+		<Button text="봉사 신청" />
+	</div>
+
+	<div class="text-2xl">어쩌구저쩌구</div>
+	<div class="text-2xl">김치찌개 맛있어요</div>
+	<div class="text-2xl">저쩌구어쩌구</div>
+	<div class="text-2xl">가나다라</div>
+	<div class="text-2xl">마바사</div>
+</div>
