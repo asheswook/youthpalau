eb914c0af87a6cbdc4899826fcd8c1bd685c03f7
Jaewook Lee <wookboy00@naver.com>
2024-03-31 18:20:57 +0900

    Feat: Button component

---
+++ src/components/button/Button.svelte
@@ -0,0 +1,12 @@
+<!-- Button component in svelte with tailwindcss-->
+<script>
+	export let text = 'Button';
+	export let onClick = () => {};
+</script>
+
+<button
+	on:click={onClick}
+	class="bg-primary-700 hover:bg-primary-600 text-white text-xl font-bold py-3 px-5 rounded"
+>
+	{text}
+</button>
