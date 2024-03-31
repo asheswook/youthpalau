33254b8b4e0aae466ad2cfb7313a2065e71a23d2
Jaewook Lee <wookboy00@naver.com>
2024-03-31 18:20:48 +0900

    Feat: Footer and Header

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<div class="p-8 bg-gray-50">
+	<div class="container w-full text-lg mx-auto text-gray-800">
+		<div class="mb-2">Copyright © 2024 청년빨라우사회적협동조합. 모든 권리 보유.</div>
+		<span class="text-gray-500"
+			>대표자: 홍길동 | 사업자등록번호: 123-45-67890 | 후원문의: 010-1234-5678</span
+		>
+		<br />
+		<span class="text-gray-500"
+			>후원 전용 계좌: 신한 123-123-456789 (예금주: 청년빨라우사회적협동조합)</span
+		>
+	</div>
+</div>
---
+++ src/components/Header.svelte
@@ -0,0 +1,6 @@
+<h1 class="text-5xl font-bold">
+	현재까지 총 <span class="text-primary-700">9,122</span> 그릇의 밥공기로
+	<br />
+	<span class="text-primary-800">청년들의</span> 배를 든든히 채웠습니다.
+</h1>
+<br />
