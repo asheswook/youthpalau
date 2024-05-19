dbb45bf2c55e9ffb4f69e249866e0ec91950a453
Jaewook Lee <wookboy00@naver.com>
2024-05-20 00:31:12 +0900

    caa

--- src/lib/server/utils/database.ts
+++ src/lib/server/utils/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { env } from '$env/dynamic/private';
 
 export const connectMongoDB = async () => {
-	const MONGO_URL = env.MONGO_URL as string;
-	await mongoose.connect(MONGO_URL);
+	// const MONGO_URL = env.MONGO_URL as string;
+	// await mongoose.connect(MONGO_URL);
 	console.log('mongoDB - CONNECTION ESTABLISHED');
 };
